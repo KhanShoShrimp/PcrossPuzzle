fc468b5d39134c9d69db8455f22e6ba03cfda573
DESKTOP-F7UA2GO\khk79 <khk7905@gmail.com>
2024-07-30 15:57:44 +0900

    no message

---
+++ Header/Base.h
@@ -0,0 +1,17 @@
+#pragma once
+
+using namespace std;
+
+#include <vector>
+#include <iostream>
+#include <cstddef>
+#include <stdexcept>
+#include <stdint.h>
+#include <string>
+#include <bitset>
+
+#include "TypeDef.h"
+#include "Byte.h"
+#include "Hint.h"
+#include "TileMap.h"
+#include "Solution.h"
